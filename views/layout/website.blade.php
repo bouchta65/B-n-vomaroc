701be4bf9b08ba5a5939de00fb95d1d8475949ea
bouchta65 <bouchtamohamed01@gmail.com>
2025-03-07 14:43:34 +0000

    mise a jour le contenu

--- views/layout/website.blade.php
+++ views/layout/website.blade.php
@@ -7,8 +7,8 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet"></script>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
   <script src="https://cdn.tailwindcss.com"></script>
-  <link rel="stylesheet" href="{{ asset('assets/css/navbar.css') }}">
-  <link rel="stylesheet" href="{{ asset('assets/css/hero.css') }}">
+  <link rel="stylesheet" href="css/navbar.css">
+  <link rel="stylesheet" href="assets/css/hero.css') }}">
 
 </head>
 
